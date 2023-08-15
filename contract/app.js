b00b1cb313e9a16b501f3f9df8475fb9c00f915e
ryo515019 <142292521+ryo515019@users.noreply.github.com>
2023-08-15 17:14:04 +0900

    Add files via upload

---
+++ contract/app.js
@@ -0,0 +1,8 @@
+// $(function(){
+//   let $textarea = $('#textarea');
+//   let lineHeight = parseInt($textarea.css('lineHeight'));
+//   $textarea.on('input',function(e){
+//     let lines = ($(this).val() + '\n').match(/\n/g).length;
+//     $(this).height(lineHeight * lines);
+//   });
+// });
